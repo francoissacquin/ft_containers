2904ee11852d8bffba036c457334c77c49667cc5
Francois SACQUIN <fsacquin@e2r11p14.clusters.42paris.fr>
2022-02-11 17:00:02 +0100

    need to finish pair and make pair

--- map/pair.hpp
+++ map/pair.hpp
@@ -29,7 +29,58 @@ public:
 		// nothing here
 	}
 
+	//ASSIGNATION OPERATOR//
+	pair &		operator=( const pair & other )
+	{
+		if (this != &other)
+		{
+			_first = other._first;
+			_second =other._second;
+		}
+		return *this;
+	}
+
+	T1			get_first( void ) //is this needed?
+	{
+		return _first;
+	}
+
+	T2			get_second( void ) // is this needed?
+	{
+		return _second;
+	}
+
 }; // end of struct pair
+
+//MAKE PAIR
+template < class T1, class T2 >
+ft::pair<T1, T2>		make_pair( T1 t, T2 u )
+{
+	return pair<T1, T2>(t, u);
+}
+
+// RELATIONAL OPERATORS
+template < class T1, class T2 >
+bool					operator==( const ft::pair<T1, T2> & lhs, const ft::pair<T1, T2> & rhs)
+{
+	if (lhs.get_first() == rhs.get_first() && lhs.get_second() == rhs.get_second())
+		return true;
+	return false;
+}
+
+template < class T1, class T2 >
+bool					operator!=( const ft::pair<T1, T2> & lhs, const ft::pair<T1, T2> & rhs)
+{
+	return (!(lhs == rhs));
+}
+
+template < class T1, class T2 >
+bool					operator<( const ft::pair<T1, T2> & lhs, const ft::pair<T1, T2> & rhs)
+{
+	if (!(lhs.get_first() < rhs.get_first()))
+		return (lhs.get_second() < rhs.get_second());
+	return (lhs.get_first() < rhs.get_first())
+}
 
 } // end of namespace
 
